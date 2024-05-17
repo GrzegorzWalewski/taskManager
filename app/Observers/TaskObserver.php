2eb91fd877f3e47a8c5a5d99f5ca327eff3c1306
Grzojda <grzojda@gmail.com>
2024-05-17 16:59:13 +0200

    added test for api

--- app/Observers/TaskObserver.php
+++ app/Observers/TaskObserver.php
@@ -8,14 +8,6 @@ use App\Notifications\TaskStatusUpdated;
 class TaskObserver
 {
     /**
-     * Handle the Task "created" event.
-     */
-    public function created(Task $task): void
-    {
-        dd('task created');
-    }
-
-    /**
      * Handle the Task "updated" event.
      */
     public function updated(Task $task): void
@@ -23,29 +15,5 @@ class TaskObserver
         if ($task->isDirty('status')) {
             $task->user->notify(new TaskStatusUpdated($task));
         }
-    }
-
-    /**
-     * Handle the Task "deleted" event.
-     */
-    public function deleted(Task $task): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Task "restored" event.
-     */
-    public function restored(Task $task): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Task "force deleted" event.
-     */
-    public function forceDeleted(Task $task): void
-    {
-        //
     }
 }
